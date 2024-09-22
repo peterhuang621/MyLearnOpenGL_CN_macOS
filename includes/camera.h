d7c6e35a257a94387901ade9964867f6aa85de3f
黄熠 <peterhuang98@huangyideMacBook-Pro.local>
2024-09-22 21:44:51 +0800

    0922

--- includes/camera.h
+++ includes/camera.h
@@ -17,19 +17,21 @@ enum Camera_Movement
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float SPEED = 2.5f;
-const float SENSITIVITY = 0.1f;
+const float SENSITIVITY = 0.08f;
 const float ZOOM = 45.0f;
 
 class Camera
 {
-public:
+  public:
     glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
     glm::vec3 Right;
     glm::vec3 WorldUp;
     float Yaw, Pitch, MovementSpeed, MouseSensitivity, Zoom;
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           float yaw = YAW, float pitch = PITCH)
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = position;
         WorldUp = up;
@@ -37,7 +39,8 @@ public:
         Pitch = pitch;
         updateCameraVectors();
     }
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = glm::vec3(posX, posY, posZ);
         WorldUp = glm::vec3(upX, upY, upZ);
@@ -65,7 +68,10 @@ public:
         return viewMatrix * posMatrix;
     }
 
-    glm::mat4 GetViewMatrix() { return glm::lookAt(Position, Position + Front, Up); }
+    glm::mat4 GetViewMatrix()
+    {
+        return glm::lookAt(Position, Position + Front, Up);
+    }
 
     void ProcessKeyboard(Camera_Movement direction, float deltaTime)
     {
@@ -107,7 +113,7 @@ public:
             Zoom = 45.0f;
     }
 
-private:
+  private:
     void updateCameraVectors()
     {
         glm::vec3 front;
