9c5e44b723650ace8832673982f5b7aa7cdc2c57
黄熠 <peterhuang98@huangyidembp.local>
2024-11-23 22:54:28 +0800

    1123

--- src/3.model_loading/model_loadding.cpp
+++ src/3.model_loading/model_loadding.cpp
@@ -4,6 +4,15 @@
 #include <mesh.h>
 #include <shader.h>
 
+struct Texture
+{
+    unsigned int id;
+    string type;
+    aiString path;
+};
+
+vector<Texture> textures_loaded;
+
 class Model
 {
   public:
@@ -58,8 +67,42 @@ class Model
 
         if (mesh->mMaterialIndex >= 0)
         {
+            aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+            vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+            textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+            vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+            textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
         }
         return Mesh(vertices, indices, textures);
     };
-    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName);
+    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName)
+    {
+        vector<Texture> textures;
+        for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+        {
+            aiString str;
+            mat->GetTexture(type, i, &str);
+            bool skip = false;
+
+            for (unsigned int j = 0; j < textures_loaded.size(); j++)
+            {
+                if (strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+                {
+                    textures.push_back(textures_loaded[j]);
+                    skip = true;
+                    break;
+                }
+            }
+            if (!skip)
+            {
+                Texture texture;
+                texture.id = TextureFromFile(str.C_Str(), directory);
+                texture.type = typeName;
+                texture.path = str;
+                textures.push_back(texture);
+                textures_loaded.push_back(texture);
+            }
+        }
+        return textures;
+    };
 };
