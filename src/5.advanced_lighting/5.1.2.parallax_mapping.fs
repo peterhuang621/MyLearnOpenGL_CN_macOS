a1ad47c36109d11e9980181eafd594f80496e958
黄熠 <peterhuang98@huangyidembp.local>
2025-02-24 20:36:48 +0800

    0224

--- src/5.advanced_lighting/5.1.2.parallax_mapping.fs
+++ src/5.advanced_lighting/5.1.2.parallax_mapping.fs
@@ -15,8 +15,33 @@ uniform vec3 lightPos,viewPos;
 uniform float heightScale;
 
 vec2 ParallaxMapping(vec2 texCoords,vec3 viewDir){
-    float height=texture(depthMap,texCoords).r;
-    return texCoords-viewDir.xy*(height*heightScale);
+    const float minLayers=8, maxLayers=32;
+    float numLayers=mix(maxLayers,minLayers,abs(dot(vec3(0.0,0.0,1.0),viewDir)));
+    float layerDepth=1.0/numLayers;
+    float currentLayerDepth=0.0;
+    vec2 P=viewDir.xy/viewDir.z*heightScale;
+    vec2 deltaTexCoords=P/numLayers;
+
+    vec2 currentTexCoords=texCoords;
+    float currentDepthMapValue=texture(depthMap,currentTexCoords).r;
+
+    while(currentLayerDepth<currentDepthMapValue){
+        currentTexCoords-=deltaTexCoords;
+        currentDepthMapValue=texture(depthMap,currentTexCoords).r;
+        currentLayerDepth+=layerDepth;
+    }
+
+    // Parallex Occlusion Mapping
+    vec2 prevTexCoords=currentTexCoords+deltaTexCoords;
+    float afterDepth=currentDepthMapValue-currentLayerDepth;
+    float beforeDepth=texture(depthMap,prevTexCoords).r-currentLayerDepth+layerDepth;
+
+    float weight=afterDepth/(afterDepth-beforeDepth);
+    vec2 finalTexCoords=prevTexCoords*weight+currentTexCoords*(1.0-weight);
+
+    // float height=texture(depthMap,texCoords).r;
+    // return texCoords-viewDir.xy*(height*heightScale);
+    return finalTexCoords;
 }
 
 void main(){
